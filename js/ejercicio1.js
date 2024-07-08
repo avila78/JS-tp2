3164f6fd325616bb02b1e9038816786eb2629dc9
Luis Avila <avilaluisfernando@hotmail.com>
2024-07-07 22:19:14 -0300

    se agrego una frase para mayores de 120 del ejercicio 1

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,6 +1,9 @@
 const edad = parseInt(prompt("Ingrese su edad"));
-if (edad > 17) {
+
+if (edad > 17 && edad <= 120) {
   document.write("Ud ya puede conducir");
+} else if (edad > 120) {
+  document.write("por favor ingrese una edad valida");
 } else {
-  document.write("Ud no puede conducir");
+  document.write("Ud todavia no puede conducir");
 }
