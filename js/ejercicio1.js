a01b946f058db9db0ce06096f1efa84971cfa5c7
Luis Avila <avilaluisfernando@hotmail.com>
2024-07-08 16:36:14 -0300

    se escribio el mensaje cuando se ingresa un caracter que no sea numero

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,9 +1,15 @@
 const edad = parseInt(prompt("Ingrese su edad"));
 
-if (edad > 17 && edad <= 120) {
+if (isNaN(edad)) {
+  document.write(
+    "Alguno de los caracteres ingresados no es correcto. Por favor ingrese su edad en números"
+  );
+} else if (edad > 17 && edad <= 100) {
   document.write("Ud ya puede conducir");
-} else if (edad > 120) {
+} else if (edad > 100) {
   document.write("por favor ingrese una edad valida");
 } else {
   document.write("Ud todavia no puede conducir");
 }
+
+isNaN(nombrevariable);
