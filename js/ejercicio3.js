b74a18a3c62f42b0d55accd8c83a836917d5e80b
Luis Avila <avilaluisfernando@hotmail.com>
2024-07-09 01:50:56 -0300

    se agrego el confirm

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,7 +1,12 @@
 /* Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
 Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp */
 
-const fraseFinal = prompt("Ingrese una palabra o frase")
+let fraseFinal = "";
 
-do{ document.write(fraseFinal)}
-while (fraseFinal = fraseFinal + fraseFinal+1)
+do {
+    const palabraIng = prompt("Ingrese una palabra o frase");
+    fraseFinal = fraseFinal + '-' + palabraIng;
+}
+while (confirm ("¿Desea continuar?"))
+
+document.write(`Frase ingresada: ${fraseFinal}`)
